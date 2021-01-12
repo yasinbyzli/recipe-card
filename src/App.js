d9fc63dd9bae878e56be4016ea2af9f3e7fd1a8d
Uğur Genç <ugur.genc33@gmail.com>
2021-01-12 21:25:46 +0300

    Props (Ödev)

--- src/App.js
+++ src/App.js
@@ -15,14 +15,30 @@ function App() {
 	const likeCount = 193;
 	const isLiked = false;
 
+	/*
+	
+	Aşağıdaki Card Componenti bir yemek tarifinin detaylarını, yazarını, beğeni sayısını ve 
+	kullanıcın tarifi beğenip beğenmediğine dair bir takım bilgileri gösteriyor.
+
+	Card Componenti yukarıda belirtilen özellikleri listelemek için belli başlı "Prop"lar almakta.
+	Bu "Prop"lar sırasıyla, author, recipe, count, liked olarak adlandırılmış durumda.
+
+	Sizin için yazar (author) prop'unu örnek olarak Card componentine aşağıda ekledik.
+
+	Bu ödevde, diğer "Prop"ları da sizin göndermeniz gerekiyor.
+
+	Prop isimlerini author, recipe, count, liked şeklinde doğru yazıldığından emin olun. :)
+
+	*/
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<Card
+					/* prop ismi = { değişken } */
 					author={recipeAuthor}
-					recipe={recipeItem}
-					count={likeCount}
-					liked={isLiked}
+
+					/*  Buraya diğer Prop'ları eklemeniz gerek.  */
 				/>
 			</header>
 		</div>
