9473a54a6cad4712e6f9fe5f6aa0afa0e34c1bbe
Yasin Beyazli <yasinbeyazli29@gmail.com>
2021-04-01 11:42:07 +0300

    recipe-card

--- src/App.js
+++ src/App.js
@@ -3,25 +3,47 @@ import Card from "./components/Card/Card";
 import food from "./assets/food.jpg"; // Tell webpack this JS file uses this image
 
 function App() {
-  const recipeAuthor = "Kodluyoruz";
-  const recipeItem = {
-    title: "Avokado Ezmeli Taco",
-    date: "12 Ocak 2021, Salı",
-    image: food,
-    description:
-      "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
-  };
-
-  const likeCount = 193;
-  const isLiked = false;
+  const recipeItems = [
+    {
+      author: "Kodluyoruz",
+      title: "Avokado Ezmeli Taco",
+      date: "12 Ocak 2021, Salı",
+      description:
+        "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+      likeCount: 193,
+      isLiked: false,
+      image: "http://localhost:3000/static/media/food.58c698ab.jpg",
+    },
+    {
+      author: "Kodluyoruz",
+      title: "İskender",
+      date: "1 Nisan 2021, Perşembe",
+      image:
+        "https://i.sozcu.com.tr/wp-content/uploads/2019/11/20/iecrop/iskender-shutterstock_16_9_1574267190-880x495.jpg",
+      description:
+        "Uludağ yaylalarında yetişen otlar ve taze kekiklerle beslenen koyun ve kuzular, iskenderin tarihi lezzetini var eden en önemli ayrıntıların başında gelir. Bu etlerin, farklı bölümlerinden bir araya gelen lezzet harmanı, pide, tereyağı, sos, yoğurt, domates, yeşil biberle zenginleşerek servis edilir.",
+      likeCount: 323,
+      isLiked: false,
+    },
+    {
+      author: "Kodluyoruz",
+      title: "Hünkar Beğendi",
+      date: "1 Nisan 2021, Perşembe",
+      image:
+        "https://finanstaksi.com/wp-content/uploads/2019/12/hunkar-begendi-tarihi-hikayesi-ve-puf-noktalari.jpg",
+      description:
+        "Hünkar beğendi, kremalı kavrulmuş patlıcan püresinin üstüne servis edilen lezzetli bir kuzu yahnisinden oluşan geleneksel bir Türk yemeğidir. Püre genellikle süt ve peynir ile kalınlaştırılırken, tüm yemek bazen domates bazlı bir sosla doldurulur veya taze doğranmış maydanozla süslenir.",
+      likeCount: 362,
+      isLiked: false,
+    },
+  ];
 
   return (
     <div className="App">
       <header className="App-header">
-        <Card
-          /* prop ismi = { değişken } */
-          author={recipeAuthor}
-        />
+        {recipeItems.map((item, index) => (
+          <Card key={index} recipe={item} />
+        ))}
       </header>
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Card.css";
-import food from "../../assets/food.jpg"; // Tell webpack this JS file uses this image
 import heartOutline from "../../assets/heart-outline.png"; // Tell webpack this JS file uses this image
 import heartFill from "../../assets/heart-fill.png"; // Tell webpack this JS file uses this image
 
 export default function Card(props) {
+  
+  const [isToggled, setIsToggled] = useState(props.recipe.isLiked);
+  const [isLikeCount, setIsLikeCount] = useState(props.recipe.likeCount);
+
+  const handleClick = () => {
+
+    setIsToggled(!isToggled);
+    
+    if (! isToggled) {
+      setIsLikeCount(isLikeCount + 1);
+    } else {
+      setIsLikeCount(isLikeCount - 1);
+    }
+    
+  }
+
+  console.log(isLikeCount);
+
   return (
     <div className="card">
       <div className="card-header">
         <div className="profile">
-          <span className="letter">{props.author[0]}</span>
+          <span className="letter">{props.recipe.author[0]}</span>
         </div>
         <div className="card-title-group">
-          <h5 className="card-title">Title</h5>
-          <div className="card-date">Date</div>
+          <h5 className="card-title">{props.recipe.title}</h5>
+          <div className="card-date">{props.recipe.date}</div>
         </div>
       </div>
-      <img className="card-image" src={food} alt="Logo" />
-      <div className="card-text">Description</div>
-      <div className="card-like-bar">
-        {props.liked ? (
+      <img className="card-image" src={props.recipe.image} alt="Logo" />
+      <div className="card-text">{props.recipe.description}</div>
+      <div className="card-like-bar" onClick={handleClick}>
+        {isToggled ? (
           <img className="card-like-icon" src={heartFill} alt="Logo" />
         ) : (
           <img className="card-like-icon" src={heartOutline} alt="Logo" />
         )}
         <div className="like-text">
-          <b>7</b> kişi bu tarifi beğendi.
+          <b>{isLikeCount}</b> kişi bu tarifi beğendi.
         </div>
       </div>
     </div>
